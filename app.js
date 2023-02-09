4a3359e612dfa016135c96832def3faa12eda90c
icres <icress26@gmail.com>
2023-02-08 18:04:24 -0700

    Refacoring and higher potential final scores

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
-let score1 = document.querySelector("#player1");
-let score2 = document.querySelector("#player2");
+const p1 = {
+    name: "Player 1",
+    score: document.querySelector("#player1"),
+    button: document.querySelector("#add1"),
+};
 
-const button1 = document.querySelector("#add1");
-const button2 = document.querySelector("#add2");
-const resetButton = document.querySelector("#reset")
+const p2 = {
+    name: "Player 2",
+    score: document.querySelector("#player2"),
+    button: document.querySelector("#add2"),
+};
+
+const resetButton = document.querySelector("#reset");
 
 const cardContent = document.querySelector(".content");
+
 const congrats = document.createElement("p");
 congrats.classList.add("title");
 
-button1.addEventListener('click', () => {
+function updateScore(player, opponent) {
     let finalScore = parseInt(document.querySelector("#finalScore").value)
-    if (!score1.style.color) {
-       score1.innerText = parseInt(score1.innerText) + 1;
-        if (score1.innerText == finalScore) {
-            score1.style.color = "green";
-            score2.style.color = "red";
-            congrats.innerText = "Player 1 Wins!";
-            cardContent.append(congrats);
-        } 
-    }    
+    player.score.innerText = parseInt(player.score.innerText) + 1;
+    if (player.score.innerText == finalScore) {
+        player.score.style.color = "green";
+        opponent.score.style.color = "red";
+        congrats.innerText = `${player.name} Wins!`;
+        cardContent.append(congrats);
+        player.button.disabled = true;
+        opponent.button.disabled = true;
+    }
+};
+
+p1.button.addEventListener('click', () => {
+    updateScore(p1, p2)
 });
 
-button2.addEventListener('click', () => {
-    let finalScore = parseInt(document.querySelector("#finalScore").value)
-    if (!score2.style.color) {
-       score2.innerText = parseInt(score2.innerText) + 1;
-        if (score2.innerText == finalScore) {
-            score2.style.color = "green";
-            score1.style.color = "red";
-            congrats.innerText = "Player 2 Wins!";
-            cardContent.append(congrats);
-        } 
-    }    
+p2.button.addEventListener('click', () => {
+    updateScore(p2, p1)
 });
 
 resetButton.addEventListener('click', () => {
-    score1.innerText = 0;
-    score2.innerText = 0;
-    score1.style.color = "";
-    score2.style.color = "";
+    p1.score.innerText = 0;
+    p2.score.innerText = 0;
+    p1.score.style.color = "";
+    p2.score.style.color = "";
     congrats.remove();
+    p1.button.disabled = false;
+    p2.button.disabled = false;
 });
